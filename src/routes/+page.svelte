e89784fb87da8e4c0b47ce2a106870fcd97f6f1e
gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2026-01-09 11:56:42 +0900

    feat: tailwindcssを導入, ページのフレームをセット

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,26 @@
   let tab = $state(0);
 </script>
 
-<main class="container">
-  <div class="tabs">
+<main class="text-neutral-800">
+  <div
+    class="
+  flex flex-row mb-4 bg-neutral-200 text-xs
+  [&>button]:px-3
+  [&>button]:py-2
+  [&>button]:bg-white
+  [&>button]:border-neutral-300
+  [&>button]:cursor-pointer
+  [&>button]:transition
+  [&>button]:duration-150
+  [&>button]:ease-in-out
+  [&>button.hover]:font-bold
+  [&>button.hover]:bg-neutral-300
+  [&>button.active]:font-bold
+  [&>button.active]:bg-white
+  [&>button.active]:border-b-2
+  [&>button.active]:border-b-blue-500
+  "
+  >
     <button onclick={() => (tab = 0)} class:active={tab === 0}>
       Analytics
     </button>
@@ -29,7 +47,7 @@
       Settings
     </button>
   </div>
-  <div class="tab-content">
+  <div>
     {#if tab === 0}
       <AnalyticsMain />
     {:else if tab === 1}
@@ -41,97 +59,4 @@
 </main>
 
 <style>
-  :root {
-    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-    font-size: 16px;
-    line-height: 24px;
-    font-weight: 400;
-
-    color: #0f0f0f;
-    background-color: #f6f6f6;
-
-    font-synthesis: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-text-size-adjust: 100%;
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-
-    a:hover {
-      color: #535bf2;
-    }
-
-    h1 {
-      text-align: center;
-    }
-
-    input,
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      color: #0f0f0f;
-      background-color: #ffffff;
-      transition: border-color 0.25s;
-      box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-    }
-
-    button {
-      cursor: pointer;
-    }
-
-    button:hover {
-      border-color: #396cd8;
-    }
-    button:active {
-      border-color: #396cd8;
-      background-color: #e8e8e8;
-    }
-
-    input,
-    button {
-      outline: none;
-    }
-
-    #greet-input {
-      margin-right: 5px;
-    }
-
-    @media (prefers-color-scheme: dark) {
-      :root {
-        color: #f6f6f6;
-        background-color: #2f2f2f;
-      }
-
-      a:hover {
-        color: #24c8db;
-      }
-
-      input,
-      button {
-        color: #ffffff;
-        background-color: #0f0f0f98;
-      }
-      button:active {
-        background-color: #0f0f0f69;
-      }
-    }
-  }
-
-  .container {
-    margin: 0;
-    padding-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-  }
 </style>
